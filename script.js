001416072f4642a7eeda8a7ac36ec00cb8d4f361
engelgart-oleg <olegengelgart@gmail.com>
2026-02-02 11:53:03 +0500

    Первый commit к первому заданию

---
+++ script.js
@@ -0,0 +1,16 @@
+alert("Hello alert!");
+console.log("Hello console log!");
+
+let title = "Заголовок";
+let screens = 20;
+let screenPrice = 33;
+let rollback = 47;
+let fullPrice = 51;
+let adaptive = 620;
+
+console.log(title);
+console.log(screens);
+console.log(screenPrice);
+console.log(rollback);
+console.log(fullPrice);
+console.log(adaptive);
