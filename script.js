4d48fa6f75e041082abd91451f22d33f25e0be8a
engelgart-oleg <olegengelgart@gmail.com>
2026-02-02 13:56:44 +0500

    Присвоил переменным значения

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let title = "First Project JS";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 33;
+let rollback = 80;
+let fullPrice = 50000;
+let adaptive = true;
+
+console.log(title);
+console.log(screens);
+console.log(screenPrice);
+console.log(rollback);
+console.log(fullPrice);
+console.log(adaptive);
