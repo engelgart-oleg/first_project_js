135c3476f38d236bfc808f5c9bd4ec7efdd0a633
engelgart-oleg <olegengelgart@gmail.com>
2026-02-02 14:43:23 +0500

    Присвоил значения переменным и вывел в консоль

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 
 let title = "First Project JS";
-let screens = "Простые, Сложные, Интерактивные";
+let screens = "Простые,Сложные,Интерактивные";
 let screenPrice = 33;
 let rollback = 80;
 let fullPrice = 50000;
 let adaptive = true;
 
-console.log(title);
-console.log(screens);
+
+console.log(typeof title);
+
+console.log(screens.length);
+
 console.log(screenPrice);
 console.log(rollback);
-console.log(fullPrice);
-console.log(adaptive);
+
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+let screensResult = screens.toLowerCase().split(",");
+console.log(screensResult);
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юани",);
+console.log("Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юани",);
+
+console.log(fullPrice * (rollback / 100));
